b9dd1da28e505b52feb92cbc6ae3390ba9bf99fd
Maxwell F. Erlich <mferlich@turing.cs.olemiss.edu>
2018-11-27 19:55:13 -0600

    added styles to preferences page

--- prefs.php
+++ prefs.php
@@ -1,5 +1,5 @@
-<!-- <! This is the Home Page for the Geneva Consistory>
-<!Includes the session functions> -->
+
+<!-- Includes the session functions> -->
 <?php require_once("session.php"); ?>
 <?php 
   #establishes DB connection and includes various global functions
@@ -11,49 +11,54 @@
   }
 
 ?>
+<!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
   <meta charset="utf-8">
-  <title>Geneva Consistory</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Preferences</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/styles.css">
   <link href="https://fonts.googleapis.com/css?family=Muli%7CRoboto:400,300,500,700,900" rel="stylesheet"></head>
+  </head>
   <body>
-    <div class="main-nav">
+  <div class="main-nav">
         <ul class="nav">
           <li class="name">Ole Miss Geneva Consistory</li>
-          <li><a href="#">Home</a></li>
+          <li><a href="home.php">Home</a></li>
           <?php
           if($_SESSION["userlevel"]==3){
           echo "<li><a href='addLogin.php'>Add User</a></li>";
           } 
           ?>
           <li><a href="logout.php">Logout</a></li>
+          <li><a href="search.php">Search</a></li>
           <li><a href="#">Contact Support</a></li>
         </ul>
     </div>
 
-    <header>
-      <h1 class="tag name">Welcome to the Geneva Consistory!</h1>
+     <header style = "min-height: 0px; background:none;">
     </header>
 
-    <main class="flex">
+
+    <h1>Preferences</h1>
+    <h2 class="section-title">Change your preferences below</h2>
+
+     <main class="flex">
       <div class="card">
-        <h2 class="card-title"><a href = "search.php">Record View</a></h2>
-        <p>Click here to view records</p>
+        <h2 class="section-title">Account Information</h2>
+        <p>Last Name: </p>
+        <p>First Name: </p>
+        <p>Email: </p>
+
       </div> 
 
       <div class="card">
-        <h2 class = "card-title"><a href = "prefs.php">Preferences</a></h2>
-        <p>Click here to update your preferences</p> 
+        <h2 class = "section-title">Change Password</h2>
+        <p>Current:</p>
+        <p>New:</p>
+        <p>Confirm New:</p> 
       </div> 
 
     </main>
-    <footer>
-      <ul>
-        <li><a href="http://olemiss.edu" target="_blank" class="social olemiss"> Ole Miss</a></li>
-      </ul>
-      <p class="copyright">Copyright 2018, Ole Miss</p>
-    </footer>
   </body>
-  </html>
+</html>
